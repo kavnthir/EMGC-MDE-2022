224a73e04432ffafeb87606397d9fd0ade5c1015
KadenMarlin <kadenamarlin@vt.edu>
2022-11-04 09:43:42 -0400

    added files

---
+++ emgc-mde.srcs/sources_1/new/lowpass.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/04/2022 09:39:06 AM
+-- Design Name: 
+-- Module Name: lowpass - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lowpass is
+    Port ( input : in STD_LOGIC;
+           output : out STD_LOGIC);
+end lowpass;
+
+architecture Behavioral of lowpass is
+
+begin
+
+
+end Behavioral;
---
+++ emgc-mde.srcs/sources_1/new/pi_output.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/04/2022 09:39:06 AM
+-- Design Name: 
+-- Module Name: pi_output - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pi_output is
+    Port ( p_input : in STD_LOGIC;
+           i_input : in STD_LOGIC;
+           output : out STD_LOGIC);
+end pi_output;
+
+architecture Behavioral of pi_output is
+
+begin
+
+
+end Behavioral;
