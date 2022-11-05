698f67d465fd57b2f8d080829c467713e8784686
Kavin Thirukonda <kavnthir@vt.edu>
2022-11-04 23:24:30 -0400

    added files & changed top level module

---
+++ emgc-mde.srcs/sources_1/new/pi_controller.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/04/2022 11:14:57 PM
+-- Design Name: 
+-- Module Name: pi_controller - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity pi_controller is
+--  Port ( );
+end pi_controller;
+
+architecture Behavioral of pi_controller is
+
+begin
+
+
+end Behavioral;
---
+++ emgc-mde.srcs/sources_1/new/rs422_interface.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11/04/2022 11:23:19 PM
+-- Design Name: 
+-- Module Name: rs422_interface - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity rs422_interface is
+--  Port ( );
+end rs422_interface;
+
+architecture Behavioral of rs422_interface is
+
+begin
+
+
+end Behavioral;
