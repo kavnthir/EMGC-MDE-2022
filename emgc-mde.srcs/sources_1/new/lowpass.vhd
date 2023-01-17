a9ee4d2805204f3e8b4864eae470c84b4b706a20
KadenMarlin <kadenamarlin@vt.edu>
2023-01-16 23:03:15 -0500

    lowpass behavioral (needs to be synchonous)

--- emgc-mde.srcs/sources_1/new/lowpass.vhd
+++ emgc-mde.srcs/sources_1/new/lowpass.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -39,7 +40,16 @@ end lowpass;
 
 architecture Behavioral of lowpass is
 -- Signal declarations (wires)
+    constant K1 : integer := 3;
+    constant K2 : integer := 3;
+    constant denom : integer := 16;
+    signal int_input, int_prev, int_output: integer;
+    
 begin
 -- Functional VHDL code (logic)
+    int_input <= TO_INTEGER(unsigned(input));
+    int_prev <= int_input;
+    int_output <= ((K1 * int_input) + (K2 * int_prev)) / denom;
+    output <= STD_LOGIC_VECTOR(TO_UNSIGNED(int_output, output'length));
 
 end Behavioral;
