4cd0c418e8850e8791979d3afb65895e74daa9b7
KadenMarlin <kadenamarlin@vt.edu>
2023-02-04 23:30:43 -0500

    completed and tested maxout timer

--- emgc-mde.srcs/sources_1/new/mac_controller.vhd
+++ emgc-mde.srcs/sources_1/new/mac_controller.vhd
@@ -1,21 +1,24 @@
 ----------------------------------------------------------------------------------
--- Company: 
+-- Company: Virginia Tech
 -- Engineer: Kaden Marlin
 -- 
 -- Create Date: 01/23/2023 01:30:57 PM
--- Design Name: 
+-- Design Name: MAC Control Module
 -- Module Name: mac_controller - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
+-- Project Name: EMGC
+-- Target Devices: -
+-- Tool Versions: -
+-- Description: Main control logic for the MAC. Senses when the angle error
+--              signal has stabilized around zero, and signals mast extension.
 -- 
--- Dependencies: 
+-- Dependencies: maxout_timer.vhd
 -- 
--- Revision:
+-- Revision: 1.0
 -- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Additional Comments: Implements logic according to design diagram,
+--                      corrections will likely be necessary.
+--
+-- !!! Currently untested
 ----------------------------------------------------------------------------------
 
 
@@ -50,8 +53,11 @@ begin
     int_x_channel <= TO_INTEGER(SIGNED(x_channel));
     int_y_channel <= TO_INTEGER(SIGNED(y_channel));
 
-    -- comparators
-    -- fix this by doing actual bit shift and compare
+    -- comparators:
+    -- x_comp_wire and y_comp_wire are '1' when
+    -- the corresponding input is < 0.5 degrees (fixed point)
+    
+    -- !!! maybe fix this by doing actual bit shift and compare instead of relying on integers
     process(clk) begin
         if (clk'event and clk = '1') then
             if (int_x_channel < 8) then
@@ -69,6 +75,8 @@ begin
     end process;        
     
     -- control for timer starting/resetting
+    -- always reset the timer to zero when comparison is false
+    -- extend mast when stable untill the limit is hit
     timer_enable <= master_enable and x_comp_wire and y_comp_wire;
     timer_reset <= not timer_enable;
     
@@ -78,7 +86,11 @@ begin
                                                max => timer_max);
                                                
     mast_extend <= master_enable and timer_max and not mast_limit;
-    -- rework this for any changes to logic
+    
+    -- !!! rework this for any changes to logic
+    -- right now, the system enable for all other modules is just
+    -- directly connected to the GPIO enable, so the controller will
+    -- continue to stabilize the mast after it is fully extended
     sys_enable <= master_enable;
     sys_reset <= not master_enable;
 
--- emgc-mde.srcs/sources_1/new/maxout_timer.vhd
+++ emgc-mde.srcs/sources_1/new/maxout_timer.vhd
@@ -1,18 +1,22 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: Virginia Tech
+-- Engineer: Kaden Marlin
 -- 
 -- Create Date: 01/23/2023 02:57:53 PM
--- Design Name: 
+-- Design Name: Max-out Timer
 -- Module Name: maxout_timer - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
+-- Project Name: EMGC
+-- Target Devices: -
+-- Tool Versions: -
 -- Description: 
+-- clock automatically ticks up its value when enabled
+-- stops counting and outputs HIGH on "max" when
+-- it reaches its maximum value
+-- resets to zero when rst is set HIGH
 -- 
--- Dependencies: 
+-- Dependencies: -
 -- 
--- Revision:
+-- Revision: 1.0
 -- Revision 0.01 - File Created
 -- Additional Comments:
 -- 
@@ -21,10 +25,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -32,6 +37,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity maxout_timer is
+    Generic ( bitsize : integer :=  8);
     Port ( clk : in STD_LOGIC;
            en : in STD_LOGIC;
            rst : in STD_LOGIC;
@@ -39,10 +45,22 @@ entity maxout_timer is
 end maxout_timer;
 
 architecture Behavioral of maxout_timer is
-    -- clock automatically ticks up its value when 
-    -- stops counting andoutputs HIGH on "max" when
-    -- it reaches its maximum value.
+    signal count : UNSIGNED(bitsize-1 downto 0) := (others => '0');
+    
 begin
-
+    
+    -- synchronus timer, asynchronus reset
+    process (clk, rst) begin
+        if (rst = '1') then
+            count <= (others => '0');
+        elsif (rising_edge(clk) and en = '1') then
+            if (count /= 2**bitsize-1) then
+                count <= count + 1;
+            end if;
+        end if;
+    end process;
+    
+    -- combinational output for max
+    max <= '1' when count = 2**bitsize-1 else '0';
 
 end Behavioral;
